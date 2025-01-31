c79bd8bfc0fe25443f1fb30f4de368ad30e31d7d
YOUNGMINKIM <Youngmin1085@gmail.com>
2025-01-31 19:25:44 +0900

    chore: Initial commit ecommerce project

---
+++ src/main/java/com/ecommerce/SpringbootEcommerceApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcommerceApplication.class, args);
+	}
+}
